e699c4956c0e9c87221f704e92942b9ca5d12656
devjosegomez <devjosegomez@gmail.com>
2019-10-18 15:30:02 -0500

    Initial commit / added StringApp Class

---
+++ src/com/exercise15strings/app/StringsApp.java
@@ -0,0 +1,11 @@
+package com.exercise15strings.app;
+
+public class StringsApp {
+
+	public static void main(String[] args) {
+		String cad1 = "";
+		cad1 = "We wish to colour the interior of each triangle with one of three colours: red, green or blue, so that no two neighbouring triangles have the same colour. Such a colouring shall be called valid. Here, two triangles are said to be neighbouring if they share an edge.";
+		
+	}
+
+}
